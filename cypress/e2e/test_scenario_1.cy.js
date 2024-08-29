f4f7ec3aa25834644a3f7b88dce0b6f48018630b
Ruta <ruta@Robertass-MacBook-Pro.local>
2024-08-29 15:57:10 +0300

    refactored scenario 1

--- cypress/e2e/test_scenario_1.cy.js
+++ cypress/e2e/test_scenario_1.cy.js
@@ -1,96 +1,108 @@
 describe("Website online shopping and checkout process", () => {
-  it("should navigate to the Hoodies & Sweatshirts section", () => {
-    //  visit the page
+  beforeEach(() => {
+    // visit the homepage
     cy.visit("https://magento.softwaretestingboard.com/");
 
-    // assert that homepage contains expected elements
+    // navigate to the Hoodies & Sweatshirts section
     cy.get("header").should("be.visible");
-    cy.get("#ui-id-5").should("be.visible").and("contain.text", "Men");
+    cy.get("#ui-id-5").trigger("mouseover");
+    cy.get("#ui-id-17").trigger("mouseover");
+    cy.get("#ui-id-20").click(); // navigate to Hoodies & Sweatshirts
+  });
 
-    // using navigation menu, find mens Hoodies & Sweatshirts section.
-    cy.get("#ui-id-5").should("be.visible").trigger("mouseover");
-    cy.get("#ui-id-17").should("be.visible").trigger("mouseover");
-    cy.get("#ui-id-20").should("be.visible").click();
+  describe("Product List", () => {
+    it("should display 12 items per page and verify the item count", () => {
+      cy.get(":nth-child(3) > .modes > .active").click();
+      cy.get(".products.list.items.product-items > li").should(
+        "have.length",
+        12
+      );
+      cy.get(":nth-child(3) > #toolbar-amount").should(
+        "contain.text",
+        "1-12 of 13"
+      );
+    });
 
-    // check/assert that the displayed number of jackets matches the selected number of jackets displayed per page
+    it("should display 10 items per page and verify the item count", () => {
+      cy.get(":nth-child(3) > .modes > #mode-list").click();
+      cy.get(".products.list.items.product-items > li").should(
+        "have.length",
+        10
+      );
+      cy.get(":nth-child(3) > #toolbar-amount").should(
+        "contain.text",
+        "1-10 of 13"
+      );
+    });
+  });
 
-    // 12 items displayed per page
-    cy.get(":nth-child(3) > .modes > .active").click();
-    cy.get(".products.list.items.product-items > li").should("have.length", 12);
-    cy.get(":nth-child(3) > #toolbar-amount").should(
-      "contain.text",
-      "1-12 of 13"
-    );
+  describe("Selecting and Adding Product to Cart", () => {
+    beforeEach(() => {
+      // Navigate to the product details page and add the product to the cart
+      cy.contains("Frankie Sweatshirt").should("be.visible").click();
+      cy.get('div[option-label="L"]').click().should("have.class", "selected");
+      cy.get('div[option-label="Yellow"]').click();
+      cy.get("#qty").clear().type("7").should("have.value", "7");
+      cy.get("#product-addtocart-button").click();
+    });
 
-    // 10 items displayed per page
-    cy.get(":nth-child(3) > .modes > #mode-list").click();
-    cy.get(".products.list.items.product-items > li").should("have.length", 10);
-    cy.get(":nth-child(3) > #toolbar-amount").should(
-      "contain.text",
-      "1-10 of 13"
-    );
+    it("should display the correct product quantity in the cart", () => {
+      cy.get(".showcart").should("contain.text", "7");
+    });
 
-    // select “Frankie Sweatshirt” and open its details
-    cy.contains("Frankie Sweatshirt").click();
+    it("should open the cart and verify the added product details", () => {
+      cy.get(".action.showcart").click();
+      cy.get("#mini-cart > .item > :nth-child(1) > .product-item-details")
+        .should("contain.text", "L")
+        .and("contain.text", "Yellow");
+    });
+  });
 
-    // select size, colour and quantity.
+  describe("Checkout Process", () => {
+    beforeEach(() => {
+      cy.get(".showcart").click(); // open the cart
+      cy.get("#top-cart-btn-checkout").should("be.visible").click(); // proceed to checkout
+    });
 
-    cy.get("div[option-label=L").click().should("have.class", "selected");
-    cy.get('div[option-label="Yellow"]').click();
-    cy.get("#qty").clear().type("7").should("have.value", "7");
+    it("should complete the shipping information form", () => {
+      cy.get("#customer-email").type("josh@email.com", { force: true });
+      cy.get('input[name="firstname"]')
+        .type("Josh")
+        .should("have.value", "Josh");
+      cy.get('input[name="lastname"]')
+        .type("Joshon")
+        .should("have.value", "Joshon");
+      cy.get('input[name="street[0]"]')
+        .type("Street 16")
+        .should("have.value", "Street 16");
+      cy.get('input[name="city"]')
+        .type("Colorado")
+        .should("have.value", "Colorado");
+      cy.get('select[name="region_id"]')
+        .select("Hawaii")
+        .should("have.value", "21");
+      cy.get('input[name="postcode"]')
+        .type("32134")
+        .should("have.value", "32134");
+      cy.get('select[name="country_id"]')
+        .select("United States")
+        .should("have.value", "US");
+      cy.get('input[name="telephone"]')
+        .type("4352094")
+        .should("have.value", "4352094");
+      cy.get('input[type="radio"][value="flatrate_flatrate"]').check();
+      cy.get("#shipping-method-buttons-container > div > button")
+        .should("be.visible")
+        .and("contain", "Next")
+        .click();
+    });
 
-    // add product to cart and check that cart icon is updated with product quantity
-    cy.get("#product-addtocart-button").click();
-    cy.get(".showcart").should("contain.text", "7");
-
-    // open cart and check if product matches the one you added to the cart
-    cy.get(".action.showcart").click();
-    // need to solve frankie sweatshirt text error
-    //   cy.get('strong.product-item-name').should('contain', 'Frankie Sweatshirt');
-    //   cy.get('#mini-cart > .item  > :nth-child(1) > .product-item-details > .product-item-name > a').should('contain', 'Frankie Sweatshirt');
-    cy.get("#mini-cart > .item > :nth-child(1) > .product-item-details")
-      .should("contain.text", "L")
-      .and("contain.text", "Yellow");
-
-    // proceed to checkout
-    cy.get("#top-cart-btn-checkout")
-      .should("be.visible")
-      .and("contain.text", "Proceed to Checkout")
-      .click();
-
-    // complete the order
-    cy.get("#customer-email")
-      .should("be.visible")
-      .type("josh@email.com", { force: true });
-    cy.get('input[name="firstname"]').type("Josh").should("have.value", "Josh");
-    cy.get('input[name="lastname"]')
-      .type("Joshon")
-      .should("have.value", "Joshon");
-    cy.get('input[name="street[0]"]')
-      .type("Street 16")
-      .should("have.value", "Street 16");
-    cy.get('input[name="city"]').type("London").should("have.value", "London");
-    cy.get('select[name="region_id"]')
-      .select("Hawaii")
-      .should("have.value", "21");
-    cy.get('input[name="postcode"]')
-      .type("32134")
-      .should("have.value", "32134");
-    cy.get('select[name="country_id"]')
-      .select("United States")
-      .should("have.value", "US");
-    cy.get('input[name="telephone"]')
-      .type("4352094")
-      .should("have.value", "4352094");
-    cy.get('input[type="radio"][value="flatrate_flatrate"]').check();
-    cy.get("#shipping-method-buttons-container > div > button")
-      .should("be.visible")
-      .and("contain", "Next")
-      .click();
-    cy.get("#billing-address-same-as-shipping-checkmo").check();
-    cy.get("button.action.primary.checkout")
-      .should("be.visible")
-      .should("not.be.disabled")
-      .click();
+    it("should complete the payment and review process", () => {
+      cy.get("#billing-address-same-as-shipping-checkmo").check();
+      cy.get("button.action.primary.checkout")
+        .should("be.visible")
+        .should("not.be.disabled")
+        .click();
+    });
   });
 });
--- cypress/e2e/test_scenario_2.cy.js
+++ cypress/e2e/test_scenario_2.cy.js
@@ -14,6 +14,8 @@ describe("Website online shopping and checkout process", () => {
     // filter section to show the cheapest products available
     cy.get("#sorter").select("Price");
 
+    cy.wait(5000);
+
     // select the cheapest pants and add them to the cart
     cy.get(
       "#maincontent > div.columns > div.column.main > div.products.wrapper.grid.products-grid > ol > li:nth-child(1) div"
@@ -22,14 +24,123 @@ describe("Website online shopping and checkout process", () => {
       .scrollIntoView()
       .trigger("mouseover", { force: true })
       .within(() => {
-        // Select the size and color options
+        // select the size and color options
         cy.get("#option-label-size-143-item-172").click(); // Select size
         cy.get("#option-label-color-93-item-52").click(); // Select color
 
-        // Hover over the "Add to Cart" button to ensure it's visible
+        // hover over the "Add to Cart" button to ensure it's visible
         cy.get('button[title="Add to Cart"]')
           .should("be.visible")
           .click({ force: true });
       });
+
+    // check that product was added to the cart
+
+    cy.get(".showcart").should("contain.text", "1");
+
+    // add 2 more products to the cart. Check that cart icon is updated with each product
+
+    // product 1
+    cy.get(
+      "#maincontent > div.columns > div.column.main > div.products.wrapper.grid.products-grid > ol > li:nth-child(2) > div"
+    )
+      .scrollIntoView()
+      .trigger("mouseover", { force: true })
+      .within(() => {
+        // select the size and color options
+        cy.get("#option-label-size-143-item-171").click(); // Select size
+        cy.get("#option-label-color-93-item-58").click(); // Select color
+
+        // hover over the "Add to Cart" button to ensure it's visible
+        cy.get('button[title="Add to Cart"]')
+          .should("be.visible")
+          .click({ force: true });
+      });
+
+    // check that product 1 was added to the cart
+    cy.get(".showcart").should("contain.text", "2");
+
+    // product 2
+    cy.get(
+      "#maincontent > div.columns > div.column.main > div.products.wrapper.grid.products-grid > ol > li:nth-child(3) > div"
+    )
+      .scrollIntoView()
+      .trigger("mouseover", { force: true })
+      .within(() => {
+        // select the size and color options
+        cy.get("#option-label-size-143-item-171").click(); // Select size
+        cy.get("#option-label-color-93-item-57").click(); // Select color
+
+        // hover over the "Add to Cart" button to ensure it's visible
+        cy.get('button[title="Add to Cart"]')
+          .should("be.visible")
+          .click({ force: true });
+      });
+
+    // check that product 2 was added to the cart
+    cy.get(".showcart").should("contain.text", "3");
+
+    // remove product from the cart
+    //removing 1st product
+    cy.get(".showcart").click();
+    cy.get(
+      "#mini-cart > li:nth-child(1) > div > div > div.product.actions > div.secondary > a"
+    ).click();
+    cy.get(
+      "body > div.modals-wrapper > aside.modal-popup.confirm._show > div.modal-inner-wrap > footer > button.action-primary.action-accept"
+    ).click();
+
+    // verify that cart shows 2 products
+    cy.get(".showcart").should("contain.text", "2");
+
+    // proceed to checkout
+    cy.get("#top-cart-btn-checkout")
+      .should("be.visible")
+      .and("contain.text", "Proceed to Checkout")
+      .click();
+
+    // add product to the cart from suggested products
+
+    // complete the order
+    // shipping info
+    cy.get("#customer-email")
+      .should("be.visible")
+      .type("joana@email.com", { force: true });
+    cy.get('input[name="firstname"]')
+      .type("Joana")
+      .should("have.value", "Joana");
+    cy.get('input[name="lastname"]')
+      .type("Joanson")
+      .should("have.value", "Joanson");
+    cy.get('input[name="street[0]"]')
+      .type("Street 27")
+      .should("have.value", "Street 27");
+    cy.get('input[name="city"]')
+      .type("Alabama")
+      .should("have.value", "Alabama");
+    cy.get('select[name="region_id"]')
+      .select("Alabama")
+      .should("have.value", "1");
+    cy.get('input[name="postcode"]')
+      .type("12345")
+      .should("have.value", "12345");
+    cy.get('select[name="country_id"]')
+      .select("United States")
+      .should("have.value", "US");
+    cy.get('input[name="telephone"]')
+      .type("786555")
+      .should("have.value", "786555");
+    cy.get('input[type="radio"][value="flatrate_flatrate"]').check();
+    cy.get("#shipping-method-buttons-container > div > button")
+      .should("be.visible")
+      .and("contain", "Next")
+      .click();
+
+    // review & payments info
+    cy.get("#billing-address-same-as-shipping-checkmo").check();
+    cy.get("button.action.primary.checkout")
+      .should("be.visible")
+      .should("not.be.disabled")
+      .click();
   });
 });
